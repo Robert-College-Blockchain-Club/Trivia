9ae50e6ae55530e21310c92b76f9920fad57abc6
Eremeyen <60618914+Eremeyen@users.noreply.github.com>
2022-10-10 21:31:57 +0300

    added questions class

---
+++ questions.js
@@ -0,0 +1,12 @@
+
+
+class questions {
+    constructor(prompt,sectionA,sectionB,sectionC,sectionD,answer) {
+        this.prompt = prompt
+        this.sectionA = sectionA
+        this.sectionB = sectionB
+        this.sectionC = sectionC
+        this.sectionD = sectionD
+        this.answer = answer //0, 1, 2, or 3 | respectively A, B, C, or D
+    }
+}
