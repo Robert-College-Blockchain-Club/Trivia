c9eb19532e70db71e999cbcd4e94d8502f4d12e8
Eremeyen <60618914+Eremeyen@users.noreply.github.com>
2022-10-15 14:17:56 +0300

    commented out, draft quiz func

--- questions.js
+++ questions.js
@@ -1,7 +1,15 @@
-class questions {
+
+export class questions {
     constructor(prompt,choices,answer) {
         this.prompt = prompt
         this.choices = choices
         this.answer = answer //0, 1, 2, or 3 | respectively A, B, C, or D
     }
+}
+
+export const generateSampleQuestion = () => {
+    let prompt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    let choices = ["A","B","C","D"]
+    let answer = Math.floor(Math.random()*3)+1
+    return new questions(prompt,choices,answer)
 }
--- trivia_script.js
+++ trivia_script.js
@@ -1,39 +1,41 @@
 // https://github.com/WebDevSimplified/JavaScript-Quiz-App/blob/master/script.js 
-
+import { questions,generateSampleQuestion } from "./questions.js"
 const startButton = document.getElementById("start-btn")
 const nextButton = document.getElementById("next-btn")
 const questionContainerElement = document.getElementById("question-container")
 const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
 startButton.addEventListener("click",startGame)
 nextButton.addEventListener("click",()=>{
-    /*
+    
     currentQuestionIndex++
     setNextQuestion()
-    */
+    
 })
+const testButton = document.getElementById("test-btn")
 
-// let shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 
-const startGame = () => {
+function startGame(){
     startButton.classList.add("hide")
-    //shuffledQuestions = questions.sort(() => Math.random()-.5)
-    //currentQuestionIndex = 0
+    shuffledQuestions = questions_list.sort(() => Math.random()-.5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove("hide")
     setNextQuestion()
 }
 
-const setNextQuestion = () => {
+function setNextQuestion(){
     resetState()
-    showQuestion(/*shuffledQuestions[currentQuestionIndex]*/)
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-const showQuestion = (question) =>{
+function showQuestion(question){
     questionElement.innerText = question.prompt //check
     question.choices.forEach(choice => {
         const button = document.createElement("button")
-        button.innerText = answer.innerTextbutton.classList.add("btn")
+        button.innerText = choice
         if(question.choices.indexOf(choice) == question.answer) {
-            button.dataset.correct = answer.correct
+            button.dataset.correct = question.choices.indexOf(choice) == question.answer
         }
         button.addEventListener("click",selectAnswer)
         answerButtonsElement.appendChild(button)
@@ -41,8 +43,7 @@ const showQuestion = (question) =>{
 
 }
 
-const selectAnswer = (e) => {
-    /*
+function selectAnswer(e){
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
     setStatusClass(document.body,correct)
@@ -50,10 +51,14 @@ const selectAnswer = (e) => {
         setStatusClass(button,button.dataset.correct)
     })
     nextButton.classList.remove("hide")
-    if()
-    */
+    if(shuffledQuestions.length > currentQuestionIndex+1){
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "restart"
+        startButton.classList.remove("hide")
+    }
 }
-const setStatusClass = (element,correct) => {
+function setStatusClass(element,correct){
     clearStatusClass(element)
     if(correct){
         element.classList.add("correct")
@@ -63,10 +68,29 @@ const setStatusClass = (element,correct) => {
     }
 }
 
-const resetState = () => {
+function resetState(){
+    clearStatusClass(document.body)
     nextButton.classList.add("hide")
-    while (answerButonsElement.firstChild){
+    while (answerButtonsElement.firstChild){
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
-const questions = []
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+
+
+const questions_list = []
+
+function populateQList(qList){
+    for(let i = 0; i <10;i++){
+        qList.push(generateSampleQuestion())
+    }
+    qList.forEach(q => {
+        console.log(q.choices)
+    }
+    )
+}
+testButton.addEventListener("click",populateQList(questions_list))
