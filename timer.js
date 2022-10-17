26170fac64a216b8d468e915768b20aa163b06a9
eremeyen <erenonder1905@gmail.com>
2022-10-17 15:06:41 +0300

    fixed bug when website was first loaded

--- timer.js
+++ timer.js
@@ -1,39 +1,50 @@
 import {startGame} from "./trivia_script.js";
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 16, 2022 01:09:40").getTime();
-let count = 0
+const right_now = new Date()
+const trivia_time_today = new Date()
+const trivia_hour = 15
+const trivia_minute = 4
+trivia_time_today.setHours(trivia_hour)
+trivia_time_today.setMinutes(trivia_minute)
+trivia_time_today.setSeconds(0)
+trivia_time_today.setUTCMilliseconds(0)
+
+//console.log("initial _date: "+ _date)
+while(right_now.getTime()>trivia_time_today.getTime()){
+  trivia_time_today.setDate(trivia_time_today.getDate()+1)
+}
+//console.log("secondary date: " + _date)
+
 // Update the count down every 1 second
-var x = setInterval(function() {
+let trivia_time_in_milliseconds = trivia_time_today.getTime()
+const x = setInterval(function() {
 
   // Get today's date and time
-  var now = new Date().getTime();
+  let now = new Date().getTime();
     
   // Find the distance between now and the count down date
-
-  var distance = countDownDate - now;
+  
+  let distance = trivia_time_in_milliseconds - now;
 
   // When countdown is finished, proceed to the next 24 h 
   if (distance <= 0) {
     // clearInterval(x);
     // document.getElementById("demo").innerHTML = "EXPIRED";
-    countDownDate += (24 * 60 * 60 * 1000);
-    distance = countDownDate - now;
+    trivia_time_in_milliseconds += (24 * 60 * 60 * 1000);
+    distance = trivia_time_in_milliseconds - now;
     // start the game if timer = 0
-    if(count !== 0){
-      console.log("yes")
-      startGame();
-    }
+    startGame()
   }
-  count = count + 1
+  //count = count + 1
   //console.log(count)
 
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
   // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
