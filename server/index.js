6a10df49b2fc419a9a2e38c2ffd96c56207c35d9
Eremeyen <60618914+Eremeyen@users.noreply.github.com>
2022-11-13 15:35:23 +0300

    added database integration to front end

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 
 const app = express();
 
+const cors = require("cors");
+
+app.use(cors());
+
 const {addUserDataPoint, deleteUserDataPoint, fetchUserDataPoint} = require("./firebaseSetup.js")
 
 //to-do: deal with errors (status codes)
-app.get("/add_user/:userID/:userScore/:userTime/:gameDay", async (req,res) => {
+app.get("/add_user/:gameDay/:userID/:userTime/:userScore", async (req,res) => {
     await addUserDataPoint(req.params.gameDay,req.params.userID,req.params.userTime,req.params.userScore);
     res.status(200)
-    res.send("Added user to database")
+    res.json("Added user to database")
 })
 
 app.listen(3000)
