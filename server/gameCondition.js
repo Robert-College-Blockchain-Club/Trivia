66ebc9af4c7ec72deae94855c17bd0f33027776a
cantogr07 <cantogran2007@gmail.com>
2022-11-16 19:34:42 +0300

    minor fixes

--- server/gameCondition.js
+++ server/gameCondition.js
@@ -11,10 +11,10 @@ const contract_abi = "";
 // The Contract object
 const contract = new ethers.Contract(contract_address, contract_abi, provider);
 
+// Checks if the user has already token(s) to start game
 function hasERC1155(address, id) { // id will be later filled in
     if (balanceOf(address, id) !== 0) {
         return true;
-
     } else {
         return false;
     }
