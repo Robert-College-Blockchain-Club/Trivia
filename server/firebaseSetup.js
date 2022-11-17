66decb0c380e8fac7d9104a122bc29c0d7040e66
eremeyen <erenonder1905@gmail.com>
2022-11-17 19:16:04 +0300

    firebase config .env

--- server/firebaseSetup.js
+++ server/firebaseSetup.js
@@ -8,14 +8,16 @@ const { doc, setDoc, deleteDoc, getDoc } = require("firebase/firestore");
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+require('dotenv').config();
+
 const firebaseConfig = {
-    apiKey: "AIzaSyACvyGUz4g4KbIFYCBJ8sXIgzTAMeBzex0",
-    authDomain: "trivia-dapp.firebaseapp.com",
-    projectId: "trivia-dapp",
-    storageBucket: "trivia-dapp.appspot.com",
-    messagingSenderId: "396046274473",
-    appId: "1:396046274473:web:897f3f61b92fceec6bbeb8",
-    measurementId: "G-S6VZCXS4YZ"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
