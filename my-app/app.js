b1b78ef9e604eb77c474f1d726740edf094b03c8
MKA06 <mertka0601@gmail.com>
2022-11-18 19:54:39 +0300

    edited mplace

--- my-app/app.js
+++ my-app/app.js
@@ -5,9 +5,11 @@ import {// tokenContract, cardcontract, signer
     mintToken, //amount input  0.01 ether tanesi
     // returns bool success above success
     seeBalance, //erc20 balance, erc1155 regular balance, erc115 premium balance
-    totalSupplyToken
+    totalSupplyToken,
+    checkConnection
 } from "./getFunctions.js";
 
+const container = document.getElementById("container");
 const connectButton = document.getElementById("connect-btn");
 const mintRegularButton = document.getElementById("mint-regular-btn");
 const mintPremiumButton = document.getElementById("mint-premium-btn");
@@ -17,28 +19,117 @@ const tokenBalance = document.getElementById("token-balance");
 const mintTokenButton = document.getElementById("mint-token-btn");
 const mintTokenInput = document.getElementById("amount-input");
 const tokenSupply = document.getElementById("token-supply");
-
+const costHolder = document.getElementById("display-cost");
+const errorPart = document.getElementById("error");
 
 connectButton.addEventListener("click", connect);
 mintTokenButton.addEventListener("click", mintTriviaToken)
+mintRegularButton.addEventListener("click", mintRegularCardFunc);
+mintPremiumButton.addEventListener("click", mintPremiumCardFunc); 
+mintTokenInput.addEventListener("input",displayCost)
+
+
+const vals = await getBalances();
+tokenBalance.innerText = ("Token Balance:" + vals[0]);
+premiumBalance.innerText = ("Premium Balance:" + vals[2]);
+regularBalance.innerText = ("Regular Balance:" + vals[1]);
+tokenSupply.innerText = ("Total Supply:" + vals[3]);
+
+async function mintRegularCardFunc() {
+    const vals = await getBalances();
+    console.log(vals)
+    if (vals[1] == 1) {
+        errorPart.innerHTML= "You cant own more than 1 TRVR"
+    } else if (vals[0] < 5) 
+    {
+        errorPart.innerHTML = "Insufficient TriviaTokens"
+    }
+    else {
+        mintRegularCard();
+    }
+}
+
+async function mintPremiumCardFunc() {
+    const vals = await getBalances();
+    console.log(vals)
+    if (vals[2] == 1) {
+        errorPart.innerHTML= "You cant own more than 1 TRVP"
+    } else if (vals[0] < 15) 
+    {
+        errorPart.innerHTML = "Insufficient TriviaTokens"
+    } 
+    else {
+        mintPremiumCard();
+    }
+}
+
+function connectionChecker() {
+    console.log(checkConnection());
+    if (checkConnection())
+    {
+        connectButton.style.display = "none";
+    } else {
+        connectButton.style.display= "block"
+    }
+}
+
+
+function isNumeric(str) {
+    if (typeof str != "string") return false 
+    return !isNaN(str) && !isNaN(parseFloat(str)) 
+  }
 
 async function mintTriviaToken() {
-    var inputVal = mintTokenInput.value;
+    var inputVal = mintTokenInput.value;    
+    const result = await mintToken(inputVal);
+    mintTokenInput.value= "";
+    if (result == "INSUFFICIENT_FUNDS") {
+        errorPart.innerHTML= "Insufficient funds"
+    }
+
+    
+
     await mintToken(inputVal);
 }
 
-mintRegularButton.addEventListener("click", mintRegularCard);
-mintPremiumButton.addEventListener("click", mintPremiumCard); 
 
+async function displayCost() {
+    const inputVal = mintTokenInput.value
+    if (isNumeric(inputVal)) 
+    {
+        const inputValInt = Number(inputVal);
+        const totalCost = inputValInt * 0.01;
+        costHolder.innerHTML= "Cost: "+  totalCost.toString();
+    } else {
+        costHolder.innerHTML = "Enter a Number"
+    }
 
-const [tokenBalanceVal, regularBalanceVal, premiumBalanceVal] = await seeBalance();
-tokenBalance.innerText = ("Token Balance:" + tokenBalanceVal);
-premiumBalance.innerText = ("Premium Balance:" + premiumBalanceVal);
-regularBalance.innerText = ("Regular Balance:" + regularBalanceVal);
-tokenSupply.innerText = ("Total Supply:" + await  totalSupplyToken());
+}
+async function getBalances() {
+    // vals[0] = tokenBalance
+    // vals[1] = regular card balance
+    // vals[2] = premium card balance
+    // vals[3] = totalSupply
+    const vals = await seeBalance();
+    for (let i = 0; i< vals.length; i++) 
+    {
+        console.log(vals[i].toString())
+        vals[i] = vals[i].toString();
+        if (vals[i].startsWith("0"))
+        {
+            vals[i] = "0";
+        } else 
+        {
+            vals[i] = vals[i].split("0").shift();
+        }
+    } 
 
+    const totalSupplyBigNum = await totalSupplyToken();
+    const totalSupplyStr = totalSupplyBigNum.toString().split("000000000000000000").shift();
+    vals.push(totalSupplyStr);
 
-
+    return vals
+}
 
 
 async function connect() {
@@ -51,3 +142,4 @@ async function connect() {
         const account = accounts[0];
         connectButton.innerText= "connected";
 } }
+
--- my-app/getFunctions.js
+++ my-app/getFunctions.js
@@ -1,17 +1,19 @@
 import { ethers } from "https://cdn-cors.ethers.io/lib/ethers-5.5.4.esm.min.js";
 import { tokenAbi,cardAbi, token_address, card_address} from "./constants/index.js";
 
+async function getContracts() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = await provider.getSigner(0);
+    console.log("can you see this man");
+    const tokenContract = new ethers.Contract(token_address, tokenAbi, signer);
+    const cardContract = new ethers.Contract(card_address, cardAbi, signer);
 
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = await provider.getSigner(0);
-console.log("can you see this man");
-const userAddress = await signer.getAddress();
-const tokenContract = new ethers.Contract(token_address, tokenAbi, signer);
-const cardContract = new ethers.Contract(card_address, cardAbi, signer);
-
+    return [tokenContract, cardContract, signer]
+}
 
 async function approveContract()  {
     try {
+        const [tokenContract, cardContract, signer] = await getContracts();
         const amountApproved = ethers.BigNumber.from("1000000000000000000000000");
         await tokenContract.approve(cardContract.address, amountApproved);
     } catch (err) {
@@ -21,6 +23,7 @@ async function approveContract()  {
 
  export async function mintRegularCard() {
     try {
+        const [tokenContract, cardContract, signer] = await getContracts();
         let success= false;
         
         await approveContract();
@@ -36,13 +39,16 @@ async function approveContract()  {
         return success;
 
     } catch (err) {
-        console.log(err);
+        console.log(err.message);
+        console.log(err)
+        console.log(typeof err)
     }
 }
 
 
  export async function mintPremiumCard() {
     try {
+        const [tokenContract, cardContract, signer] = await getContracts();
 
         let success= false;
         
@@ -65,27 +71,25 @@ async function approveContract()  {
 
  export async function mintToken(_amount){
     try { 
-        //amount to be an int 
+        const [tokenContract, cardContract, signer] = await getContracts();
+
         const mintAmount = _amount.toString();
         const etherNeededInt = 0.01 * Number(_amount);
         const etherNeeded = { value: ethers.utils.parseEther(etherNeededInt.toString())};
-               
+
         let tx = await tokenContract.mint(mintAmount, etherNeeded);
         await tx.wait();
         
-        if (await tokenContract.balanceOf(signer.getAddress()) === mintAmount)
-        {
-            console.log("minted tokens with success");
-        } else {
-            console.log("Unsuccesful mint,  debug");
-        } 
     } catch (err) {
-        console.log(err);
+        return err.code;
     }
 }
 
  export async function seeBalance() {
+
     try {
+        const [tokenContract, cardContract, signer] = await getContracts();
+        console.log(signer)
         var tokenBalance;
         var regularCardBalance;
         var premiumCardBalance;
@@ -102,12 +106,25 @@ async function approveContract()  {
 
  export async function totalSupplyToken() {
     try {
+        const [tokenContract, cardContract, signer] = await getContracts();
+
         var totalSupply = await tokenContract.totalSupply();
         return totalSupply;
 
     } catch (err) {
         console.log(err);
     }
+}
+
+export async function checkConnection() {
+        const accounts  = ethereum.request({ method: 'eth_accounts' }); 
+        console.log("isundefined", accounts[0] == undefined)
+        if ((accounts[0]==undefined)) 
+        {
+            return false;
+        } else {
+            return true;
+        }
 }
 
 
